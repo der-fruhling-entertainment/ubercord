0a5d1fdd73bc3e09a1442ed6677eefef1507edf4
der_frühling <daedula@derfruhling.net>
2025-05-23 23:28:52 -0700

    update config

--- mod/common/src/main/java/net/derfruhling/minecraft/ubercord/server/ServerConfig.java
+++ mod/common/src/main/java/net/derfruhling/minecraft/ubercord/server/ServerConfig.java
@@ -1,38 +1,58 @@
 package net.derfruhling.minecraft.ubercord.server;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.derfruhling.minecraft.ubercord.DisplayConfig;
-import net.derfruhling.minecraft.ubercord.DisplayMode;
-import net.derfruhling.minecraft.ubercord.client.ClientConfig;
-import net.minecraft.client.Minecraft;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.storage.LevelResource;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 public record ServerConfig(
         long clientId,
+        UUID serverId,
         @Nullable String botToken,
         @Nullable DisplayConfig display,
+        @Nullable String authDomain,
         @Nullable String authKey
 ) {
-    public static @Nullable ServerConfig loadMaybe() {
-        Path configFile = Path.of("config").resolve("ubercord-server.json");
+    private static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+
+    public ServerConfig() {
+        this(1367390201621512313L, UUID.randomUUID(), null, null, null, null);
+    }
+
+    public static ServerConfig loadOrDefault(MinecraftServer server) {
+        Path configFile = server.getWorldPath(LevelResource.ROOT).resolve("serverconfig").resolve("ubercord-server.json");
         if (!Files.exists(configFile)) {
-            return null;
+            ServerConfig cfg = new ServerConfig();
+
+            try {
+                cfg.save(server);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return cfg;
         } else {
             try {
-                return new Gson().fromJson(Files.readString(configFile), ServerConfig.class);
+                return GSON.fromJson(Files.readString(configFile), ServerConfig.class);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
-    public void save() throws IOException {
-        Path configDir = Path.of("config");
-        Files.writeString(configDir.resolve("ubercord-server.json"), new Gson().toJson(this), StandardCharsets.UTF_8);
+    public void save(MinecraftServer server) throws IOException {
+        Path configDir = server.getWorldPath(LevelResource.ROOT).resolve("serverconfig");
+        Files.createDirectories(configDir);
+        Files.writeString(configDir.resolve("ubercord-server.json"), GSON.toJson(this), StandardCharsets.UTF_8);
     }
 }
